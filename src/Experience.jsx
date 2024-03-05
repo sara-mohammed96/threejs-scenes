97ab87b797d39d52248990b318a4a4567a88c32e
Sarah Allateef <sara.korea96@gmail.com>
2024-03-05 19:27:26 +0300

    setup the app

---
+++ src/Experience.jsx
@@ -0,0 +1,16 @@
+import { shaderMaterial, Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
+import { useFrame, extend } from '@react-three/fiber'
+import { useRef } from 'react'
+
+
+
+export default function Experience()
+{
+   
+    return <>
+
+       
+
+    </>
+}
